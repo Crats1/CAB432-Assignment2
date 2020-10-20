fc301ad814a25c469f509bf9683b49a13be0cae8
ekulz6 <luke_maley@hotmail.com>
2020-10-21 02:21:09 +1000

    fetch tweets to home page
    created functions in home.js to fetch tweets from serverside and display them to page

---
+++ public/javascripts/home.js
@@ -0,0 +1,32 @@
+// Assign query to global var after input from user is
+var query;
+
+function getInput() {
+    var queryInput = 
+        document.querySelector("#searchForm [name='search']");
+    query = queryInput.value;
+    document.getElementById('tweets').innerHTML = "Searching for: " + query + "<br>";
+    
+}
+
+function fetchTweets() {
+    url=`api/search?words=${query}`;
+    return fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+
+        for(let i=0; i < data.statuses.length; i++){
+            screenName = data.statuses[i].user.screen_name
+            userName = data.statuses[i].user.name
+            tweet = data.statuses[i].text
+
+            console.log(screenName)
+            console.log(userName)
+            document.getElementById('tweets').innerHTML += "<b>" + screenName + "</b><br>" + tweet + "<br>"   
+        }
+    })
+}
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", getInput);
+submitButton.addEventListener("click", fetchTweets);
